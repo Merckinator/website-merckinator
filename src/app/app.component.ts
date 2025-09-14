30979133f7384c18b170f60a250a6cd1e43d0694
Jeff Merckens <jeff.merckens@gmail.com>
2025-09-14 08:20:32 -0400

    feat: replace ant design with just pico css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { NzAlertModule } from 'ng-zorro-antd/alert';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'merck-root',
-  imports: [RouterModule, RouterOutlet, NzLayoutModule, NzMenuModule, NzAlertModule],
+  imports: [RouterModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
