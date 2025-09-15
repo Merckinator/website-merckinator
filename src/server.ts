569aed535f4dc5e00ae6483de18bd25d50eb80df
Jeff Merckens <jeff.merckens@gmail.com>
2025-09-15 07:03:04 -0400

    fix: add host to server.ts for Railway

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ app.use(
     maxAge: '1y',
     index: false,
     redirect: false,
-  }),
+  })
 );
 
 /**
@@ -41,9 +41,7 @@ app.use(
 app.use((req, res, next) => {
   angularApp
     .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
+    .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
     .catch(next);
 });
 
@@ -52,8 +50,8 @@ app.use((req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
+  const port = parseInt(process.env['PORT'] ?? '') || 4000;
+  app.listen(port, '0.0.0.0', (error) => {
     if (error) {
       throw error;
     }
