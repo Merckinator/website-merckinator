d14e4eed70f74681f6c883bb0ccdb0248211ad13
Jeff Merckens <jeff.merckens@gmail.com>
2025-09-15 06:57:00 -0400

    fix: adjust server route so Welcome prerenders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 export const routes: Routes = [
   {
-    path: '**',
+    path: '',
     loadComponent: () =>
       import('./pages/welcome/welcome.component').then((m) => m.WelcomeComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
